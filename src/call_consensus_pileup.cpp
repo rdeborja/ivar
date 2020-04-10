e18a9ada8b9d28b57ee5afe7ad9359a7aa6ed25a
gkarthik <xraydiffraction@gmail.com>
2020-04-09 21:14:06 -0700

    Changed where vcf_writer initialized

--- src/call_consensus_pileup.cpp
+++ src/call_consensus_pileup.cpp
@@ -135,9 +135,10 @@ int call_consensus_from_plup(std::istream &cin, std::string out_file, uint8_t mi
   std::ofstream fout((out_file+".fa").c_str());
   std::ofstream tmp_qout((out_file+".qual.txt").c_str());
   char *o = new char[out_file.length() + 1];
-  vcf_writer *vw = new vcf_writer('b', out_file, basename(o), ref_path);
+  char *sample_name = basename(o);
+  vcf_writer *vw = NULL;
   strcpy(o, out_file.c_str());
-  fout << ">Consensus_" << basename(o) << "_threshold_" << threshold << "_quality_" << (uint16_t) min_qual  <<std::endl;
+  fout << ">Consensus_" << sample_name << "_threshold_" << threshold << "_quality_" << (uint16_t) min_qual  <<std::endl;
   delete [] o;
   int ctr = 0, mdepth = 0;
   uint32_t prev_pos = 0, pos = 0;
@@ -184,13 +185,16 @@ int call_consensus_from_plup(std::istream &cin, std::string out_file, uint8_t mi
       fout << std::string((pos - prev_pos) - 1, gap);
       tmp_qout << std::string((pos - prev_pos) - 1, '!'); // ! represents 0 quality score.
     }
-    if(vw->get_region().empty()) // Note: for now consensus working only for 1 region
-      vw->add_hdr_region(region);
+    if(vw == NULL){ // Note: for now consensus working only for 1 region
+      if(!ref_path.empty())
+	vw = new vcf_writer('b', out_file+".bcf", region, basename(o), ref_path);
+    }
     ret_t t;
     if(mdepth >= min_depth){
       ad = update_allele_depth(ref, bases, qualities, min_qual);
       t = get_consensus_allele(ad, min_qual, threshold, gap);
-      vw->write_record(pos, ad, ref, threshold);
+      if(vw!=NULL)
+	vw->write_record(pos, ad, region, ref, threshold);
       fout << t.nuc;
       tmp_qout << t.q;
     } else{
