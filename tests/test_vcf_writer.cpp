d142dd65cf7219b060820a54336fdae59373eba0
gkarthik <xraydiffraction@gmail.com>
2020-04-14 21:23:49 -0700

    Added more tests

--- tests/test_vcf_writer.cpp
+++ tests/test_vcf_writer.cpp
@@ -48,6 +48,74 @@ int main(int argc, char *argv[])
   write_test_vcf(ad, ref);
   write_test_vcf(ad, 'T');
   success += check_vcf("test_vcf\t30\t.\tTTGC\tATG,G\t6\tPASS\tDP=1,7;AD=1,7;ADF=-1,-1;ADR=2,8;GQ=3,9;AF=0.0833333,0.583333\tGT:AD:GQ:ADF:ADR:AF\t1|0|2:1,4,7:3,6,9:-1,-1,-1:2,5,8:0.0833333,0.333333,0.583333");
+
+  ad.clear();
+  ad = {{
+      "A",
+      1,
+      2,
+      3
+    },{
+      "T",
+      4,
+      5,
+      6
+    }, {
+      "G",
+      7,
+      8,
+      9,
+      0,
+      0,
+      0,
+      "TGC"
+    }};
+  write_test_vcf(ad, ref);
+  write_test_vcf(ad, 'A');
+  success += check_vcf("test_vcf\t30\t.\tATGC\tT,G\t3\tPASS\tDP=4,7;AD=4,7;ADF=-1,-1;ADR=5,8;GQ=6,9;AF=0.333333,0.583333\tGT:AD:GQ:ADF:ADR:AF\t0|1|2:1,4,7:3,6,9:-1,-1,-1:2,5,8:0.0833333,0.333333,0.583333");
+
+
+  ad.clear();
+  ad = {{
+      "A",
+      1,
+      2,
+      3,
+      0,
+      0,
+      0,
+      ""
+    },{
+      "T",
+      4,
+      5,
+      6,
+      0,
+      0,
+      0,
+      ""
+    }, {
+      "G",
+      7,
+      8,
+      9,
+      0,
+      0,
+      0,
+      ""
+    }, {
+      "GTA",
+      7,
+      8,
+      9,
+      0,
+      0,
+      0,
+      ""
+    }};
+  write_test_vcf(ad, ref);
+  write_test_vcf(ad, 'G');
+  success += check_vcf("test_vcf\t30\t.\tG\tA,T,GTA\t9\tPASS\tDP=1,4,7;AD=1,4,7;ADF=-1,-1,-1;ADR=2,5,8;GQ=3,6,9;AF=0.0526316,0.210526,0.368421\tGT:AD:GQ:ADF:ADR:AF\t1|2|0|3:1,4,7,7:3,6,9,9:-1,-1,-1,-1:2,5,8,8:0.0526316,0.210526,0.368421,0.368421");
   return success;
 }
 
