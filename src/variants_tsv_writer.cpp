e404c55a0559e21fee20b57cdd548c9acdedb45d
gkarthik <xraydiffraction@gmail.com>
2020-04-20 21:55:19 -0700

    Added tsv_writer

--- src/variants_tsv_writer.cpp
+++ src/variants_tsv_writer.cpp
@@ -3,11 +3,23 @@
 const float sig_level = 0.01;
 
 variants_tsv_writer::variants_tsv_writer(std::string path){
-  this->fout.open((path+".tsv").c_str());
+  this->fout.open(path.c_str());
   if(this->fout.fail()){
     std::cout << "Failed to open file " << path << std::endl;
   }
-  this->fout << this->hdr << std::endl;
+}
+
+void variants_tsv_writer::add_ref(ref_antd *ref_a){
+  this->refa = ref_a;
+  if(this->refa->is_empty()){
+    this->fout << this->hdr << std::endl;
+  } else {
+    this->fout << this->hdr_aa << std::endl;
+  }
+}
+
+variants_tsv_writer::~variants_tsv_writer(){
+  this->fout.close();
 }
 
 int variants_tsv_writer::write_line(allele a, allele ref, std::string region, uint64_t pos, double *freq_depth){
@@ -16,19 +28,19 @@ int variants_tsv_writer::write_line(allele a, allele ref, std::string region, ui
   out_str << region << this->delim;
   out_str << pos << this->delim;
   if(!(a.deleted_bases.empty())){
-    out_str << ref.nuc << a.deleted_bases << "\t";
+    out_str << ref.nuc << a.deleted_bases << this->delim;
   } else {
-    out_str << ref.nuc << "\t";
+    out_str << ref.nuc << this->delim;
   }
-  out_str << a.nuc << "\t";
-  out_str << ref.depth << "\t";
-  out_str << ref.reverse << "\t";
-  out_str << (uint8_t) ref.mean_qual << "\t";
-  out_str << a.depth << "\t";
-  out_str << a.reverse << "\t";
-  out_str << (uint8_t) a.mean_qual << "\t";
-  out_str << freq_depth[0] << "\t";
-  out_str << freq_depth[1] << "\t";
+  out_str << a.nuc << this->delim;
+  out_str << ref.depth << this->delim;
+  out_str << ref.reverse << this->delim;
+  out_str << (uint16_t) ref.mean_qual << this->delim;
+  out_str << a.depth << this->delim;
+  out_str << a.reverse << this->delim;
+  out_str << (uint16_t) a.mean_qual << this->delim;
+  out_str << freq_depth[0] << this->delim;
+  out_str << freq_depth[1] << this->delim;
   err = pow(10, ( -1 * (a.mean_qual)/10));
   /*
         | Var   | Ref      |
@@ -36,17 +48,31 @@ int variants_tsv_writer::write_line(allele a, allele ref, std::string region, ui
     Obs | AD    | RD       |
   */
   kt_fisher_exact((err * freq_depth[1]), (1-err) * freq_depth[1], a.depth, ref.depth, &pval_left, &pval_right, &pval_twotailed);
-  out_str << pval_left << "\t";
+  out_str << pval_left << this->delim;
   if(pval_left <= sig_level){
-    out_str << "TRUE" << "\t";
+    out_str << "TRUE";
   } else {
-    out_str << "FALSE" << "\t";
+    out_str << "FALSE";
   }
-  // if(!is_indel(*it)){ // Ignore indels for aa translation
-  //   refantd.codon_aa_stream(region, out_str, fout, pos, it->nuc[0]);
-  // } else {
-  //   fout << out_str.str() << "NA\tNA\tNA\tNA\tNA" << std::endl;
-  // }  
+  if(!this->refa->is_empty()){
+    out_str << this->delim;
+    if(!is_indel(a)){ // Ignore indels for aa translation
+      std::vector<std::vector<std::string>> codon_aa = refa->codon_aa(region, pos, a.nuc[0]);
+      for (std::vector<std::vector<std::string>>::iterator it = codon_aa.begin(); it != codon_aa.end(); ++it) {
+	fout << out_str.str();
+	for (std::vector<std::string>::iterator cit = (*it).begin(); cit != (*it).end(); ++cit) {
+	  this->fout << *cit;
+	  if (cit < (it->end() - 1))
+	    this->fout << this->delim;
+	}
+	this->fout << std::endl;
+      }
+    } else {
+      this->fout << out_str.str() << "NA\tNA\tNA\tNA\tNA" << std::endl;
+    }
+  } else {
+    this->fout << out_str.str() << std::endl;
+  }
   out_str.str("");
   out_str.clear();
   return 0;
--- src/variants_tsv_writer.h
+++ src/variants_tsv_writer.h
@@ -2,10 +2,30 @@
 #include <fstream>
 #include <sstream>
 #include "allele_functions.h"
+#include "ref_seq.h"
 #include <cmath>
 #include <htslib/kfunc.h>
 
 class variants_tsv_writer{
+const std::string hdr_aa = "REGION"
+  "\tPOS"
+  "\tREF"
+  "\tALT"
+  "\tREF_DP"
+  "\tREF_RV"
+  "\tREF_QUAL"
+  "\tALT_DP"
+  "\tALT_RV"
+  "\tALT_QUAL"
+  "\tALT_FREQ"
+  "\tTOTAL_DP"
+  "\tPVAL"
+  "\tPASS"
+  "\tGFF_FEATURE"
+  "\tREF_CODON"
+  "\tREF_AA"
+  "\tALT_CODON"
+  "\tALT_AA";
 const std::string hdr = "REGION"
   "\tPOS"
   "\tREF"
@@ -27,9 +47,11 @@ const std::string hdr = "REGION"
   "\tALT_AA";
   const std::string delim = "\t";
   std::ofstream fout;
+  ref_antd *refa;
 public:
   variants_tsv_writer(std::string path);
   ~variants_tsv_writer();
   int write_line(allele a, allele ref, std::string region, uint64_t pos, double *freq_depth);
   int is_valid();
+  void add_ref(ref_antd *ref_a);
 };
