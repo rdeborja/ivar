c59de2d88367865d8a8e18a43698eebac9e946a5
gkarthik <xraydiffraction@gmail.com>
2020-04-29 18:07:58 -0700

    Added quality check

--- src/contamination.cpp
+++ src/contamination.cpp
@@ -16,16 +16,17 @@ void identify_amp(std::vector<primer> &primers, uint64_t pos, uint64_t end_pos, 
   }
 }
 
-var_by_amp* get_alleles_per_read(bam1_t *aln, var_by_amp *v, primer *fwd, primer *rev){
+var_by_amp* get_alleles_per_read(bam1_t *aln, var_by_amp *v, primer *fwd, primer *rev, uint8_t min_qual){
   std::vector<allele> all;
-  int ncigar = aln->core.n_cigar;
+  uint ncigar = aln->core.n_cigar;
   uint32_t* cigar = bam_get_cigar(aln);
   uint32_t cig;
-  int i = 0, len, ctr = 0;
+  uint i = 0, len, ctr = 0;
   uint64_t pos = aln->core.pos, ql=0;
   uint8_t *seq = bam_get_seq(aln);
   allele *a;
   std::string indel;
+  uint8_t *qual = bam_get_qual(aln), q;
   if(v == NULL)
     v = new var_by_amp(pos);
   while (i < ncigar ) {
@@ -34,9 +35,12 @@ var_by_amp* get_alleles_per_read(bam1_t *aln, var_by_amp *v, primer *fwd, primer
     if ((bam_cigar_type(cigar[i]) & 1) && (bam_cigar_type(cigar[i]) & 2)){ // Query and ref consuming
       ctr = 0;
       while(ctr < len -1){
-	v = v->get_or_add_node(pos);
-	a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
-	a->depth += 1;
+	q = qual[ql+ctr];
+	if(q >= min_qual){
+	  v = v->get_or_add_node(pos);
+	  a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
+	  a->depth += 1;
+	}
 	ctr++;
 	pos++;
       }
@@ -69,16 +73,22 @@ var_by_amp* get_alleles_per_read(bam1_t *aln, var_by_amp *v, primer *fwd, primer
 	  ql += len;				  // Add query length for previous cigar
 	  i++;
 	} else {
-	  v = v->get_or_add_node(pos);
-	  a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
-	  a->depth += 1;
+	  q = qual[ql+ctr];
+	  if(q >= min_qual){
+	    v = v->get_or_add_node(pos);
+	    a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
+	    a->depth += 1;
+	  }
 	  pos++;
 	  ql += len;
 	}
       } else {
-	v = v->get_or_add_node(pos);
-	a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
-	a->depth += 1;
+	q = qual[ql+ctr];
+	if(q >= min_qual){
+	  v = v->get_or_add_node(pos);
+	  a = v->get_or_add_allele(std::string(1, seq_nt16_str[bam_seqi(seq, ql+ctr)]), "", fwd, rev);
+	  a->depth += 1;
+	}
 	pos++;
 	ql+=len;
       }
@@ -92,28 +102,47 @@ var_by_amp* get_alleles_per_read(bam1_t *aln, var_by_amp *v, primer *fwd, primer
   return v;
 }
 
-int main()
-{
-  std::string bed = "../data/amp_sim/amp_primers.bed";
-  std::string bam = "../data/amp_sim/amp_sim.sorted.bam";
+int identify_contamination(std::string bed, std::string bam, std::string pairs_file, uint8_t min_qual){
   std::vector<primer> primers = populate_from_file(bed);
-  populate_pair_indices(primers, "../data/amp_sim/pair_information.tsv");
+  populate_pair_indices(primers, pairs_file);
   samFile *in = hts_open(bam.c_str(), "r");
-  bam_hdr_t *header = sam_hdr_read(in);
+  if(in == NULL) {
+    std::cout << ("Unable to open BAM file.") << std::endl;
+    return -1;
+  }
+  //Load the index
   hts_idx_t *idx = sam_index_load(in, bam.c_str());
+  if(idx == NULL) {
+    std::cout << "Building BAM index" << std::endl;
+    if(sam_index_build2(bam.c_str(), 0, 0)< 0){
+      std::cout << ("Unable to open or build BAM index.") << std::endl;
+      return -1;
+    } else {
+      idx = sam_index_load(in, bam.c_str());
+    }
+  }
+  bam_hdr_t *header = sam_hdr_read(in);
+  if(header == NULL) {
+    sam_close(in);
+    std::cout << "Unable to open BAM header." << std::endl;
+  }
   hts_itr_t *iter = NULL;
   std::vector<primer*> fwd, rev;
   primer *fwd_max_end, *rev_min_start;
   iter  = sam_itr_querys(idx, header, header->target_name[0]);
   var_by_amp *cur= NULL;
   bam1_t *aln = bam_init1();
+  double min_freq = 0.03;
+  std::vector<std::vector<uint32_t>> overlap = get_overlap_pos(primers);
   while(sam_itr_next(in, iter, aln) >= 0) {
     identify_amp(primers, aln->core.pos, bam_endpos(aln), fwd, rev);
-    if(fwd.size() == 0 || rev.size() == 0 || ((aln->core.flag&BAM_FUNMAP) != 0))
+    if(fwd.size() == 0 || rev.size() == 0 || ((aln->core.flag&BAM_FUNMAP) != 0)){
+      std::cout << "Reads going over two amplicons: "  << bam_get_qname(aln) << std::endl;
       continue;
+    }
     fwd_max_end = get_max_end_(fwd);
     rev_min_start = get_min_start_(rev);
-    cur = get_alleles_per_read(aln, cur, fwd_max_end, rev_min_start);
+    cur = get_alleles_per_read(aln, cur, fwd_max_end, rev_min_start, min_qual);
   }
   if(cur == NULL){
     std::cout << "No reads from amplicons found" << std::endl;
@@ -121,8 +150,41 @@ int main()
     while(cur->get_prev() != NULL){
       cur = cur->get_prev();
     }
-    cur->print_graph();
-    delete cur;
+    // Check for variants in primer positions
+    var_by_amp *tmp;
+    std::vector<allele*> alleles;
+    for (std::vector<primer>::iterator it=primers.begin(); it != primers.end(); ++it) {
+      for (uint32_t i = it->get_start(); i < it->get_end()+1; ++i) {
+	tmp = cur->get_node(i);
+	if (tmp== NULL)
+	  continue;
+	alleles = tmp->get_alleles_above_freq(min_freq);
+	if(alleles.size() >= 1)
+	  tmp->print_graph(min_freq);
+      }
+    }
+    std::cout << "Overlap" << std::endl << std::endl;
+    // Check for variants in overlap position
+    std::vector<allele*> unique_alleles;
+    std::vector<uint32_t> counts;
+    for (std::vector<std::vector<uint32_t>>::iterator it = overlap.begin(); it!=overlap.end(); ++it) {
+      for (uint32_t i = (*it).at(0); i < (*it).at(1)+1; ++i) {
+	tmp = cur->get_node(i);
+	if (tmp== NULL)
+	  continue;
+	tmp->get_distinct_variants_amp(min_freq, unique_alleles, counts);
+	for (uint j = 0; j < unique_alleles.size(); ++j) {
+	  if(counts.at(j) == tmp->get_fwd_primers().size() || tmp->get_fwd_primers().size() == 1)
+	    continue;
+	  std::cout << i << "\t"
+		    << unique_alleles.at(j)->nuc << unique_alleles.at(j)->deleted_bases << "\t"
+		    << counts.at(j)
+		    << std::endl;
+	}
+      }
+    }
+    // cur->print_graph(true);
+    delete cur; 
   }
   hts_itr_destroy(iter);
   hts_idx_destroy(idx);
--- src/contamination.h
+++ src/contamination.h
@@ -9,4 +9,4 @@
 #include "variants_by_amplicon.h"
 
 void identify_amp(std::vector<primer> &primers, uint64_t pos, uint64_t end_pos, std::vector<primer*> &fwd_primers, std::vector<primer*> &rev_primers);
-
+int identify_contamination(std::string bed, std::string bam, std::string pairs_file, uint8_t min_qual);
