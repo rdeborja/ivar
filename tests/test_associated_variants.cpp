79b280019ccd0e53be57a58e2633947c99465b46
gkarthik <xraydiffraction@gmail.com>
2020-05-10 20:15:52 -0700

    Added test for associated variant counts

--- tests/test_associated_variants.cpp
+++ tests/test_associated_variants.cpp
@@ -22,7 +22,12 @@ int main()
     cur = get_alleles_per_read(aln, cur, p1, p2, min_qual);
   }
   cur = cur->get_node(1181);
+  success += (cur->get_associated_variants().at(0)[1182]["TTGT"] == 1) ? 0 : 1;
+  success += (cur->get_associated_variants().at(0)[1183]["G-NN"] == 1) ? 0 : 1;
+  success += (cur->get_associated_variants().at(0)[1289]["A"] == 2) ? 0 : 1;
+  cur = cur->get_node(1716);
   cur->print_graph(false);
+  cur->print_linked_variants();
   delete p1;
   delete p2;
   return success;
